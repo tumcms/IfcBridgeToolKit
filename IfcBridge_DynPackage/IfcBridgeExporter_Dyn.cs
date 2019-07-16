44309f9facd193fae6560383f7135fcae0475264
TUBVCMS-SES-3-W\Sebastian Esser <s.esser@tum.de>
2019-07-16 14:19:40 +0200

    Update Nugets

--- IfcBridge_DynPackage/IfcBridgeExporter_Dyn.cs
+++ IfcBridge_DynPackage/IfcBridgeExporter_Dyn.cs
@@ -2,9 +2,19 @@
 {
     public class IfcBridgeExporter_Dyn
     {
-        public static string SayHello(string Name)
+        //public static string SayHello(string Name)
+        //{
+        //    return "Hello " + Name + "!";
+        //}
+
+
+        /// <summary>
+        /// Defines base content in IFC Model -> calls initModel
+        /// </summary>
+        /// <param name="filepath"></param>
+        public static bool InitIfcModel(string filepath)
         {
-            return "Hello " + Name + "!";
+            return true; // everything went well - otherwise receive a false
         }
     }
 }
