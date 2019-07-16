ee60b526517bff861c5693c9e71b79f1f51af3e6
TUBVCMS-SES-3-W\Sebastian Esser <s.esser@tum.de>
2019-07-16 08:43:32 +0200

    initial Setup for Dynamo Integration - no dependencies yet

---
+++ IfcBridge_DynPackage/IfcBridgeExporter_Dyn.cs
@@ -0,0 +1,10 @@
+﻿namespace IfcBridge_DynPackage
+{
+    public class IfcBridgeExporter_Dyn
+    {
+        public static string SayHello(string Name)
+        {
+            return "Hello " + Name + "!";
+        }
+    }
+}
