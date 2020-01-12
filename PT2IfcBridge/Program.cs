87185fb9dfc0a50493c4d63e0e5a88b70661377d
Sebastian Esser@XPS15_9570 <sebastian.esser@tum.de>
2020-01-12 12:05:55 +0100

    Init PT App

---
+++ PT2IfcBridge/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PT2IfcBridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" --- PointCloud 2 IfcBridge Model --- \n");
+
+            var input = Console.ReadKey();
+
+        }
+    }
+}
