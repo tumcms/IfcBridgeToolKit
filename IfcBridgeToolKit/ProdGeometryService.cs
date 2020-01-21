5646ec5cc9cee6bdd94ebb3e8c902828aeef24c2
Sebastian_Esser@TUBVCMS-SES-3-W <sebastian.esser@tum.de>
2020-01-21 11:13:06 +0100

    Integrated new geomServices from PlanPro2IFC Implementation new nuget package for STL geometries

--- IfcBridgeToolKit/ProdGeometryService.cs
+++ IfcBridgeToolKit/ProdGeometryService.cs
@@ -5,6 +5,8 @@ using Xbim.IfcRail.TopologyResource;
 using Xbim.IfcRail.GeometryResource;
 using Xbim.IfcRail.RepresentationResource;
 using Xbim.Ifc;
+using QuantumConcepts.Formats.StereoLithography;
+using Xbim.IfcRail.MeasureResource;
 
 namespace IfcBridgeToolKit
 {
@@ -67,5 +69,109 @@ namespace IfcBridgeToolKit
             //  return ProductDefinitionShape
             return ifcProductDefinitonShape;
         }
+
+        /// <summary>
+        /// Converts a given STL geometry into an IfcFacetedBRep representation
+        /// </summary>
+        /// <param name="model"></param>
+        /// <param name="stlDocument"></param>
+        /// <returns></returns>
+        public static IfcFacetedBrep StlToIfc_FBRep(ref IfcStore model, STLDocument stlDocument)
+        {
+            // ToDo: introduce Scaling: mm - m and vice versa
+            var scalingFactor = 1 / 1000000;
+
+            // STL -> IfcFacetedBRep
+            var ifcFacetedBRep = model.Instances.New<IfcFacetedBrep>();
+
+            var ifcClosedShell = model.Instances.New<IfcClosedShell>();
+            ifcFacetedBRep.Outer = ifcClosedShell;
+
+            foreach (var facet in stlDocument.Facets.ToList())
+            {
+                // create IfcPolyLoop, which holds the vertex polygon
+                var polyloop = model.Instances.New<IfcPolyLoop>();
+
+                // get pts and map them into IfcCartesianPoints
+                var pts = facet.Vertices.ToList();
+
+                foreach (var pt in pts)
+                {
+                    var ifcPt = model.Instances.New<IfcCartesianPoint>(vtx =>
+                    {
+                        vtx.SetXYZ(pt.X, pt.Y, pt.Z);
+                    });
+                    // add vertex to polyloop
+                    polyloop.Polygon.Add(ifcPt);
+                }
+
+                // create IfcFaceOuterBound instance and add the polyloop on it
+                var outerBound = model.Instances.New<IfcFaceOuterBound>(ob => { ob.Bound = polyloop; });
+
+                // Add Holes and Openings as IfcFaceBound and append it also to IfcFace
+
+
+                // add bound to IfcFace
+                var face = model.Instances.New<IfcFace>(fc => { fc.Bounds.Add(outerBound); });
+
+                // Add IfcFace instance to IfcClosedShell Instance
+                ifcClosedShell.CfsFaces.Add(face);
+            }
+
+            return ifcFacetedBRep;
+        }
+
+        /// <summary>
+        /// Extracts BRep geometry out of a given STLDocument, adds required instances to the IfcModel and returns an fcTriangulatedFaceSet instance to the model
+        /// </summary>
+        /// <param name="model"></param>
+        /// <param name="stlDocument"></param>
+        /// <returns></returns>
+        public static IfcTriangulatedFaceSet StlToIfc_TFS(ref IfcStore model, STLDocument stlDocument, double? scaling)
+        {
+            double scalingFactor = 0.001;
+            if (scaling != null)
+            {
+                scalingFactor = (double)scaling;
+            }
+
+            var pointList = model.Instances.New<IfcCartesianPointList3D>();
+            // https://github.com/xBimTeam/XbimEssentials/issues/70
+
+            var tfs = model.Instances.New<IfcTriangulatedFaceSet>(fs =>
+            {
+                fs.Closed = true;
+                fs.Coordinates = pointList;
+            });
+
+            foreach (var facet in stlDocument.Facets)
+            {
+                var facetPoints = facet.Vertices.Select(a => new { a.X, a.Y, a.Z }).ToList();
+
+                // ToDo: check, if the point is already stored and get the indices
+                var currentEndCoords = pointList.CoordList.Count;
+                var currentEndIndex = tfs.CoordIndex.Count;
+
+                var indices = new int[3];
+                for (int i = 0; i < facetPoints.Count; i++)
+                {
+                    pointList.CoordList.GetAt(currentEndCoords + i).AddRange(new IfcLengthMeasure[]
+                    {
+                        facetPoints[i].X * scalingFactor,
+                        facetPoints[i].Y * scalingFactor,
+                        facetPoints[i].Z * scalingFactor
+                    });
+
+                    indices[i] = currentEndCoords + 1 + i;
+
+                }
+                tfs.CoordIndex.GetAt(currentEndIndex).AddRange(indices.Select(j => new IfcPositiveInteger(j)));
+            }
+
+            // https://github.com/xBimTeam/XbimEssentials/issues/182 
+
+            return tfs;
+        }
+              
     }
 }
